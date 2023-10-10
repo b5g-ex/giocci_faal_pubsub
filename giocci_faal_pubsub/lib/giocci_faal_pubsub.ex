e9790f51a313bdf6cd24523b44551a8e68ecc842
fomfom <a>
2023-10-10 18:44:56 +0900

    can pubsub

--- giocci_faal_pubsub/lib/giocci_faal_pubsub.ex
+++ giocci_faal_pubsub/lib/giocci_faal_pubsub.ex
@@ -17,17 +17,23 @@ defmodule GiocciFaalPubsub do
   end
 
   def start_faal_pubsub do
-    NifZenoh.tester_sub ("demo/example/zenoh-rs-pub")
+    # NifZenoh.tester_sub ("demo/example/zenoh-rs-pub")
     # session = Zenohex.open
     # {:ok, publisher} = Session.declare_publisher(session, "demo/example/zenoh-rs-pub")
     # Publisher.put(publisher, "Hello zenoh?")
   end
-  def publish (session)do
-    {:ok, publisher} = Session.declare_publisher(session, "demo/example/zenoh-rs-pub")
+  def publish(session)do
+    {:ok, publisher} = Session.declare_publisher(session, "to/engine")
     Publisher.put(publisher, "Hello zenoh?")
   end
   def subscribe do
     session = Zenohex.open
-    Session.declare_subscriber(session, "demo/example/zenoh-rs-pub", fn m -> IO.inspect(m) end)
+    Session.declare_subscriber(session, "to/faal", fn m-> callback(m,session) end)
+
+  end
+
+  defp callback(m,session) do
+    IO.puts(m)
+    publish(session)
   end
 end
