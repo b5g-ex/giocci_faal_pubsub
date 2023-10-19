6fb55bb9b693747161d89278f6c162e425d925f7
fomfom <a>
2023-10-19 17:47:57 +0900

    this has error

--- giocci_faal_pubsub/lib/giocci_faal_pubsub.ex
+++ giocci_faal_pubsub/lib/giocci_faal_pubsub.ex
@@ -17,23 +17,40 @@ defmodule GiocciFaalPubsub do
   end
 
   def start_faal_pubsub do
-    # NifZenoh.tester_sub ("demo/example/zenoh-rs-pub")
-    # session = Zenohex.open
+    session = Zenohex.open
+    Session.declare_subscriber(session, "to/faal", fn m-> callback(m,session) end)
+    # Session.declare_subscriber(session, "return_from/engine", fn m-> callbackrt(m,session) end)
+
     # {:ok, publisher} = Session.declare_publisher(session, "demo/example/zenoh-rs-pub")
     # Publisher.put(publisher, "Hello zenoh?")
   end
-  def publish(session)do
+
+  def publish (session)do
     {:ok, publisher} = Session.declare_publisher(session, "to/engine")
     Publisher.put(publisher, "Hello zenoh?")
   end
-  def subscribe do
-    session = Zenohex.open
-    Session.declare_subscriber(session, "to/faal", fn m-> callback(m,session) end)
 
+
+
+  # def subscribe do
+  #   session = Zenohex.open
+  #   Session.declare_subscriber(session, "to/faal", fn m-> callback(m,session) end)
+
+  # end
+
+  def return_publish(session)do
+    {:ok, publisher} = Session.declare_publisher(session, "to/engine")
+    Publisher.put(publisher, "receive sucessfully")
   end
 
   defp callback(m,session) do
     IO.puts(m)
-    publish(session)
+    return_publish(session)
   end
+  defp callbackrt(m,session) do
+    IO.puts(m)
+    # return_publish(session)
+  end
+
+
 end
