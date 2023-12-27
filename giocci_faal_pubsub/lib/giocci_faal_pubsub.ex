2f4c119647926ed13577016a2c50f4e1ff9ec205
fomfom <a>
2023-12-28 04:58:56 +0900

    RENAME SOME DATA

--- giocci_faal_pubsub/lib/giocci_faal_pubsub.ex
+++ giocci_faal_pubsub/lib/giocci_faal_pubsub.ex
@@ -1,22 +1,16 @@
 defmodule GiocciFaalPubsub do
   GenServer
   @moduledoc """
-  Documentation for `GiocciFaalPubsub`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> GiocciFaalPubsub.hello()
-      :world
+  Documentation for `GiocciFaalPubsub`.\
+  faalサーバーのpubsub機構をシミュレートするモジュールです
 
   """
+
+
 
   def start_faal_pubsub do
     session = Zenohex.open
-    Session.declare_subscriber(session, "to/faal", fn m-> callback(m,session) end)
+    Session.declare_subscriber(session, "from/engine", fn m-> callback(m,session) end)
     # Session.declare_subscriber(session, "return_from/engine", fn m-> callbackrt(m,session) end)
 
     # {:ok, publisher} = Session.declare_publisher(session, "demo/example/zenoh-rs-pub")
@@ -24,22 +18,17 @@ defmodule GiocciFaalPubsub do
   end
 
   def publish (session)do
-    {:ok, publisher} = Session.declare_publisher(session, "to/engine")
+    {:ok, publisher} = Session.declare_publisher(session, "from/faal")
     Publisher.put(publisher, "Hello zenoh?")
   end
 
 
 
-  # def subscribe do
-  #   session = Zenohex.open
-  #   Session.declare_subscriber(session, "to/faal", fn m-> callback(m,session) end)
 
+  # def return_publish(session)do
+  #   {:ok, publisher} = Session.declare_publisher(session, "from/faal")
+  #   Publisher.put(publisher, "receive sucessfully")
   # end
-
-  def return_publish(session)do
-    {:ok, publisher} = Session.declare_publisher(session, "to/engine")
-    Publisher.put(publisher, "receive sucessfully")
-  end
 
   defp callback(m,session) do
     msg = m |> String.trim
